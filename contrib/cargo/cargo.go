bee06cf8003065c1c188efb3f83913ae380a124e
aarnav <aarnavbos@gmail.com>
2022-08-04 16:42:37 +0200

    add cargo.Lockfile struct

--- contrib/cargo/cargo.go
+++ contrib/cargo/cargo.go
@@ -1,1 +1,13 @@
 package cargo
+
+type LockfilePackage struct {
+	Name         string
+	Version      string
+	Source       string
+	Checksum     string
+	Dependencies []string
+}
+
+type Lockfile struct {
+	Package []LockfilePackage
+}
