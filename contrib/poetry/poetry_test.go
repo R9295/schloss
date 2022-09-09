b40923f5a3f3309b3faa768226f5e205b3c4f581
aarnav <aarnavbos@gmail.com>
2022-09-09 15:28:10 +0200

    poetry: refactor tests

--- contrib/poetry/poetry_test.go
+++ contrib/poetry/poetry_test.go
@@ -1,145 +1,138 @@
 package poetry
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/R9295/schloss/core"
+	"github.com/brianvoe/gofakeit/v6"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func getRandomName() string {
+	return strings.Replace(
+		fmt.Sprintf("%s-%s", gofakeit.HipsterWord(), gofakeit.Animal()),
+		" ",
+		"-",
+		-1,
+	)
+}
+
+func getRandomDependency() (string, string) {
+	return getRandomName(), gofakeit.AppVersion()
+}
+func getRandLockfilePkg() LockfilePackage {
+	pkg := LockfilePackage{
+		Name:         getRandomName(),
+		Version:      gofakeit.AppVersion(),
+		Dependencies: map[string]interface{}{},
+	}
+	depsAmount := gofakeit.IntRange(0, 15)
+	for i := 0; i < depsAmount; i++ {
+		name, version := getRandomDependency()
+		pkg.Dependencies[name] = version
+	}
+	return pkg
+}
+
 func TestPoetryCollectPackage(t *testing.T) {
+	pkgOne := getRandLockfilePkg()
+	pkgTwo := getRandLockfilePkg()
 	lockfile := Lockfile{Package: []LockfilePackage{
-		{
-			Name:    "parserkiosk",
-			Version: "0.3.0",
-			Dependencies: map[string]interface{}{
-				"Jinja2":     "",
-				"PyYAML":     "",
-				"python-box": "",
-				"yamale":     "",
-			},
-		},
-		{
-			Name:         "black",
-			Version:      "22.6.0",
-			Dependencies: map[string]interface{}{},
-		},
+		pkgOne,
+		pkgTwo,
 	}}
 	packages, _ := collectPackages(&lockfile)
-	assert.Equal(t, packages["parserkiosk"], lockfile.Package[0])
+	assert.Equal(t, packages[pkgOne.Name], lockfile.Package[0])
+	assert.Equal(t, packages[pkgTwo.Name], lockfile.Package[1])
+
 }
 
 func TestDiffPackagesPackageVersion(t *testing.T) {
-	oldPkg := LockfilePackage{
-		Name:    "parserkiosk",
-		Version: "0.3.0",
-	}
-
-	newPkg := LockfilePackage{
-		Name:    "parserkiosk",
-		Version: "0.3.1",
-	}
+	oldPkg := getRandLockfilePkg()
+	newPkg := oldPkg
+	newPkg.Version = "666.666"
 	diffList := make([]core.Diff, 0)
 	diffPackages(&oldPkg, &newPkg, &diffList)
 	assert.Equal(t, len(diffList), 1)
 	assert.Equal(t, diffList[0], core.MakeDependencyFieldDiff(
-		"parserkiosk", "version", oldPkg.Version, newPkg.Version,
+		oldPkg.Name, "version", oldPkg.Version, newPkg.Version,
 	))
 }
 
 func TestDiffPackagesRemovePackage(t *testing.T) {
+	pkgOne := getRandLockfilePkg()
+	pkgTwo := getRandLockfilePkg()
 	oldLockfile := Lockfile{Package: []LockfilePackage{
-		{
-			Name:         "jinja2",
-			Version:      "42.0",
-			Dependencies: map[string]interface{}{},
-		},
-		{
-			Name:         "black",
-			Version:      "22.6.0",
-			Dependencies: map[string]interface{}{},
-		},
+		pkgOne,
+		pkgTwo,
 	}}
 	newLockfile := Lockfile{Package: []LockfilePackage{
-		{
-			Name:         "jinja2",
-			Version:      "42.0",
-			Dependencies: map[string]interface{}{},
-		},
+		pkgOne,
 	}}
 	var diffList []core.Diff
 	DiffLockfiles(&oldLockfile, &newLockfile, &diffList, "rootPkg")
 	assert.Equal(t, len(diffList), 1)
-	assert.Equal(t, diffList[0], core.MakeRemovedDependencyDiff("black"))
+	assert.Equal(t, diffList[0], core.MakeRemovedDependencyDiff(pkgTwo.Name))
 }
 
 func TestDiffPackagesAddPackage(t *testing.T) {
+	pkgOne := getRandLockfilePkg()
+	pkgTwo := getRandLockfilePkg()
 	oldLockfile := Lockfile{Package: []LockfilePackage{
-		{
-			Name:         "jinja2",
-			Version:      "42.0",
-			Dependencies: map[string]interface{}{},
-		},
+		pkgOne,
 	}}
 	newLockfile := Lockfile{Package: []LockfilePackage{
-		{
-			Name:         "jinja2",
-			Version:      "42.0",
-			Dependencies: map[string]interface{}{},
-		},
-		{
-			Name:         "black",
-			Version:      "22.6.0",
-			Dependencies: map[string]interface{}{},
-		},
+		pkgOne,
+		pkgTwo,
 	}}
 	var diffList []core.Diff
 	DiffLockfiles(&oldLockfile, &newLockfile, &diffList, "rootPkg")
 	assert.Equal(t, len(diffList), 1)
-	assert.Equal(t, diffList[0], core.MakeAddedDependencyDiff("black", "22.6.0", "rootPkg"))
+	assert.Equal(
+		t,
+		diffList[0],
+		core.MakeAddedDependencyDiff(pkgTwo.Name, pkgTwo.Version, "rootPkg"),
+	)
 }
 
 func TestDiffPackagesPackageRemoveSubDependency(t *testing.T) {
-	old := LockfilePackage{
-		Name:    "jinja2",
-		Version: "42.0",
-		Dependencies: map[string]interface{}{
-			"MarkupSafe": ">=2.0",
-		},
-	}
-	new := LockfilePackage{
-		Name:         "jinja2",
-		Version:      "42.0",
+	oldPkg := getRandLockfilePkg()
+	newPkg := LockfilePackage{
+		Name:         oldPkg.Name,
+		Version:      oldPkg.Version,
 		Dependencies: map[string]interface{}{},
 	}
+	for k, v := range oldPkg.Dependencies {
+		newPkg.Dependencies[k] = v
+	}
+	oldPkg.Dependencies["to-remove"] = "0.1"
 	diffList := make([]core.Diff, 0)
-	diffPackages(&old, &new, &diffList)
+	diffPackages(&oldPkg, &newPkg, &diffList)
 	assert.Equal(t, len(diffList), 1)
-	assert.Equal(t, diffList[0], core.MakeRemovedSubDependencyDiff("MarkupSafe", "jinja2"))
+	assert.Equal(t, diffList[0], core.MakeRemovedSubDependencyDiff("to-remove", oldPkg.Name))
 }
 
 func TestDiffPackagesPackageAddSubDependency(t *testing.T) {
-	old := LockfilePackage{
-		Name:         "jinja2",
-		Version:      "42.0",
+	oldPkg := getRandLockfilePkg()
+	newPkg := LockfilePackage{
+		Name:         oldPkg.Name,
+		Version:      oldPkg.Version,
 		Dependencies: map[string]interface{}{},
 	}
-	new := LockfilePackage{
-		Name:    "jinja2",
-		Version: "42.0",
-		Dependencies: map[string]interface{}{
-
-			"MarkupSafe": ">=2.0",
-		},
+	for k, v := range oldPkg.Dependencies {
+		newPkg.Dependencies[k] = v
 	}
+	newPkg.Dependencies["added"] = "0.1"
 	diffList := make([]core.Diff, 0)
-	diffPackages(&old, &new, &diffList)
+	diffPackages(&oldPkg, &newPkg, &diffList)
 	assert.Equal(t, len(diffList), 1)
 	assert.Equal(t, diffList[0], core.MakeAddedSubDependencyDiff(
-		"MarkupSafe",
-		"jinja2",
-		">=2.0",
+		"added",
+		oldPkg.Name,
+		"0.1",
 	))
 }
 
