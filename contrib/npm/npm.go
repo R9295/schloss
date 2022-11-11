e3a0e25ee49db514d2987dbf58fd41e1afdf158e
Emely Henninger <hee3be@bosch.com>
2022-11-11 18:14:21 +0100

    fix subdependency diff

--- contrib/npm/npm.go
+++ contrib/npm/npm.go
@@ -9,9 +9,8 @@ import (
 type Lockfile struct {
 	Name            string                     `json:"name"`
 	Version         string                     `json:"version"`
-	LockfileVersion int                        `json:"lockfileVersion"`
+	LockfileVersion string                     `json:"lockfileVersion"`
 	Packages        map[string]LockfilePackage `json:"packages"`
-	SubPackages     map[string]LockfilePackage `json:"dependencies"`
 }
 
 type LockfilePackage struct {
@@ -32,42 +31,74 @@ func parseJson(lockfile string) (Lockfile, error) {
 
 func collectPackages(lockFile *Lockfile, diffList *[]core.Diff) {
 	for pkgName, pkg := range lockFile.Packages {
-		for _, subPkg := range pkg.Dependencies {
-			pgkSubdependency, exists := lockFile.SubPackages[subPkg]
+		for _, depName := range pkg.Dependencies {
+			subPkg, exists := lockFile.Packages[depName]
 			// find subdependencies in json object dependencies
 			if exists {
-				pgkSubdependency.ParentPackages = append(pgkSubdependency.ParentPackages, pkgName)
-				lockFile.SubPackages[subPkg] = pgkSubdependency
-			} else {
-				*diffList = append(*diffList, core.MakeRemovedSubDependencyDiff(subPkg, pkgName))
+				subPkg.ParentPackages = append(subPkg.ParentPackages, pkgName)
 			}
+			// also in diffPackagesSubdependencies
+			/* else {
+				*diffList = append(*diffList, core.MakeRemovedSubDependencyDiff(depName, pkgName))
+			} */
 		}
 	}
 
 }
 
-func diffPackages(oldPkgName string, oldPkg LockfilePackage, newPkg LockfilePackage, diffList *[]core.Diff) {
+func diffPackageFields(
+	oldPkgName string,
+	oldPkg LockfilePackage,
+	newPkg LockfilePackage,
+	diffList *[]core.Diff,
+) {
 	// check for changed fields
 	if oldPkg.Version != newPkg.Version {
-		*diffList = append(*diffList, core.MakeDependencyFieldDiff(oldPkgName, "version", oldPkg.Version, newPkg.Version))
+		*diffList = append(
+			*diffList,
+			core.MakeDependencyFieldDiff(oldPkgName, "version", oldPkg.Version, newPkg.Version),
+		)
 	}
 	if oldPkg.Integrity != newPkg.Integrity {
-		*diffList = append(*diffList, core.MakeDependencyFieldDiff(oldPkgName, "integrity", oldPkg.Integrity, newPkg.Integrity))
+		*diffList = append(
+			*diffList,
+			core.MakeDependencyFieldDiff(
+				oldPkgName,
+				"integrity",
+				oldPkg.Integrity,
+				newPkg.Integrity,
+			),
+		)
 	}
 	if oldPkg.Resolved != newPkg.Resolved {
-		*diffList = append(*diffList, core.MakeDependencyFieldDiff(oldPkgName, "resolved", oldPkg.Resolved, newPkg.Resolved))
+		*diffList = append(
+			*diffList,
+			core.MakeDependencyFieldDiff(oldPkgName, "resolved", oldPkg.Resolved, newPkg.Resolved),
+		)
 	}
-	// check if subdendency List matches, quick check if list is empty
-	for _, oldPkgDep := range oldPkg.Dependencies {
+}
+
+func diffPackageSubDependencies(
+	oldPkgName string,
+	oldPkg LockfilePackage,
+	newPkg LockfilePackage,
+	diffList *[]core.Diff,
+) {
+
+	for oldPkgDep, _ := range oldPkg.Dependencies {
 		exists := false
 		for _, newPkgDep := range newPkg.Dependencies {
 			if newPkgDep == oldPkgDep {
 				exists = true
+				delete(newPkg.Dependencies, newPkgDep)
 			}
 		}
 		if !exists {
 			*diffList = append(*diffList, core.MakeRemovedSubDependencyDiff(oldPkgDep, oldPkgName))
 		}
+	}
+	for newPkgDep, newPkgVersion := range newPkg.Dependencies {
+		*diffList = append(*diffList, core.MakeAddedSubDependencyDiff(newPkgDep, oldPkgName, newPkgVersion))
 	}
 }
 
@@ -84,34 +115,30 @@ func DiffLockfiles(
 		if !exists {
 			*diffList = append(*diffList, core.MakeRemovedDependencyDiff(oldPkgName))
 		} else {
-			diffPackages(oldPkgName, oldPkg, newPkg, diffList)
+			diffPackageFields(oldPkgName, oldPkg, newPkg, diffList)
+			diffPackageSubDependencies(oldPkgName, oldPkg, newPkg, diffList)
 			delete(newLockfile.Packages, oldPkgName)
 		}
 	}
 	for newPackageName, newPkg := range newLockfile.Packages {
 		*diffList = append(*diffList, core.MakeAddedDependencyDiff(newPackageName, newPkg.Version, newLockfile.Name))
-	}
-	// check differences in SubPackages
-	for oldSubPkgName, oldSubPkg := range oldLockfile.SubPackages {
-		newSubPkg, exists := newLockfile.SubPackages[oldSubPkgName]
-		if !exists {
-			// for every parent make a sub dependency diff => double diff entry because of diffPackages()?
-			for _, parentPkgName := range oldSubPkg.ParentPackages {
-				*diffList = append(*diffList, core.MakeRemovedSubDependencyDiff(oldSubPkgName, parentPkgName))
-			}
+		/* if sliceContains(newPkg.ParentPackages, newLockfile.Name) && len(newPkg.ParentPackages) == 1 {
+			*diffList = append(*diffList, core.MakeAddedDependencyDiff(newPackageName, newPkg.Version, newLockfile.Name))
 		} else {
-			diffPackages(oldSubPkgName, oldSubPkg, newSubPkg, diffList)
-			delete(newLockfile.Packages, oldSubPkgName)
-		}
-	}
-	for newSubPkgName, newSubPkg := range newLockfile.SubPackages {
-		// for every parent
-		for _, parentPkg := range newSubPkg.ParentPackages {
-			*diffList = append(*diffList, core.MakeAddedSubDependencyDiff(newSubPkgName, parentPkg, newSubPkg.Version))
-		}
+			*diffList = append(*diffList, core.MakeAddedSubDependencyDiff(newPackageName, newPkg.Version, newLockfile.Name))
+		} */
 
 	}
 }
+
+/* func sliceContains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+} */
 
 func Diff(rootFile *string, oldLockfile *string, newLockfile *string, diffList *[]core.Diff) error {
 	oldLockfileJson, err := parseJson(*oldLockfile)
@@ -125,3 +152,13 @@ func Diff(rootFile *string, oldLockfile *string, newLockfile *string, diffList *
 	DiffLockfiles(&oldLockfileJson, &newLockfileJson, diffList)
 	return nil
 }
+
+/* func removeStringOfSlice(slice []string, removeString string) []string {
+	newSlice := []string{}
+	for _, element := range slice {
+		if element != removeString {
+			newSlice = append(newSlice, element)
+		}
+	}
+	return newSlice
+} */
